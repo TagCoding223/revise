7d62ad9fd93bae00e96bb0839d3a58d9fc8622a6
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-19 15:37:20 +0530

    UserService
    define the service interface for UserService

---
+++ backend/src/main/java/com/revise/service/UserService.java
@@ -0,0 +1,14 @@
+package com.revise.service;
+
+import com.revise.dto.request.CreateUserRequest;
+import com.revise.dto.response.UserResponse;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponse createUser(CreateUserRequest request);
+
+    UserResponse getUserById(String id);
+
+    List<UserResponse> getAllUsers(); // remove or admin secure this
+}
