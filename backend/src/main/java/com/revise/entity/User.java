d0b4166ff0034a2e945425ae311dc569acee407c
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-19 15:10:51 +0530

    initial api testing
    define a UserController and test /api/v1/user/test endpoint and define user entity

---
+++ backend/src/main/java/com/revise/entity/User.java
@@ -0,0 +1,40 @@
+package com.revise.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data // Lombok generates getters, setters, and toString
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, nullable = false,  columnDefinition = "VARCHAR(36)")
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    // e.g., 'LOCAL' or 'GOOGLE'
+    @Column(name = "auth_Provider")
+    private String authProvider;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
