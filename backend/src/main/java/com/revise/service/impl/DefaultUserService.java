73dd132b607ba0ac479353f497e154d6b74ae44c
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-19 16:14:52 +0530

    UserAuthService
    Define the UserAuthService in Impl folder of services.

---
+++ backend/src/main/java/com/revise/service/impl/DefaultUserService.java
@@ -0,0 +1,62 @@
+package com.revise.service.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.revise.dto.request.CreateUserRequest;
+import com.revise.dto.response.UserResponse;
+import com.revise.entity.User;
+import com.revise.repository.UserRepository;
+import com.revise.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+// Never use the suffix Impl on your classes. It is an anti-pattern because it leaks implementation details. Name your implementation based on its specific domain, such as UserService (interface) and DatabaseUserService or DefaultUserService (class).
+// so i change the UserServiceImpl to DefaultUserService (to define general operation related to user and authentication in AuthUserService)
+
+@Service
+@RequiredArgsConstructor
+public class DefaultUserService implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserResponse createUser(CreateUserRequest request) {
+        // 1. Map DTO to Entity
+        User user = new User();
+        user.setEmail(request.getEmail());
+        user.setFullName(request.getFullName());
+        user.setAuthProvider(request.getAuthProvider() != null ? request.getAuthProvider() : "LOCAL");
+        
+        // 2. Save to Database via Repository
+        User savedUser = userRepository.save(user);
+
+        // 3. Map Entity back to Response DTO
+        return mapToResponse(savedUser);
+    }
+
+    @Override
+    public UserResponse getUserById(String id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found with id: "+ id));
+
+        return mapToResponse(user);
+    }
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream().map(this::mapToResponse).collect(Collectors.toList());
+    }
+    
+    // Helper method to keep mapping logic clean
+    private UserResponse mapToResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setEmail(user.getEmail());
+        response.setFullName(user.getFullName());
+        response.setAuthProvider(user.getAuthProvider());
+        response.setCreatedAt(user.getCreatedAt());
+        return response;
+    }
+}
