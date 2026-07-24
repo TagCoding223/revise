55c0648f37750c7c90ad30990a658962ae5150f7
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-24 21:23:54 +0530

    (backend) add createUser endpoint in UserController to check user creation servies, dto and repository

--- backend/src/main/java/com/revise/controller/UserController.java
+++ backend/src/main/java/com/revise/controller/UserController.java
@@ -2,16 +2,32 @@ package com.revise.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.revise.dto.request.CreateUserRequest;
+import com.revise.dto.response.UserResponse;
+import com.revise.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+    
     @GetMapping("/test")
     public String test() {
         return new String("Worked");
+    }
+    
+    @PostMapping("/createUser")
+    public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
+        UserResponse createdUser = userService.createUser(request);
+        return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
     }
     
 }
