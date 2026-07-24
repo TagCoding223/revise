de3b286bc5b6a8fe8c3dcb9172f5601d4d61445e
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-24 21:35:07 +0530

    (backend) add getUserById endpoint.

--- backend/src/main/java/com/revise/controller/UserController.java
+++ backend/src/main/java/com/revise/controller/UserController.java
@@ -9,12 +9,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/v1/user")
-@RequiredArgsConstructor
+@RequiredArgsConstructor // what is done and how it is allow to access userServiceImpl class method to link with final userService field
 public class UserController {
 
     private final UserService userService;
@@ -28,6 +31,11 @@ public class UserController {
     public ResponseEntity<UserResponse> createUser(@RequestBody CreateUserRequest request) {
         UserResponse createdUser = userService.createUser(request);
         return new ResponseEntity<>(createdUser,HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable String id) {
+        return ResponseEntity.ok(userService.getUserById(id));
     }
     
 }
