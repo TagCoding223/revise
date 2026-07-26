882319fb23b75a86c602d275655e64883541402a
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-26 12:28:41 +0530

    (backend) Create Handler for InvaildOtpException and ResourceNotFoundException

--- backend/src/main/java/com/revise/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/revise/exception/GlobalExceptionHandler.java
@@ -7,20 +7,37 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.revise.dto.response.ApiResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     // This catches our custom exception and returns a 409 Conflict status
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ApiResponse> handleUserAlreadyExists(UserAlreadyExistsException ex){
         ApiResponse response = new ApiResponse(false, ex.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT); // 409
     }
 
+    // Handle bad OTP submission
+    @ExceptionHandler(InvalidOtpException.class)
+    public ResponseEntity<ApiResponse> handleInvalidOtp(InvalidOtpException ex){
+        ApiResponse response = new ApiResponse(false, ex.getMessage());
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST); // 400
+    }
 
+    // Handle missing records (User or OTP)
+    public ResponseEntity<ApiResponse> handleResourceNotFound(ResourceNotFoundException ex){
+        ApiResponse response = new ApiResponse(false, ex.getMessage());
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND); // 404
+    }
+
+    // Keep this at the bottom to catch actual server crashes
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleGeneralException(Exception ex){
-        ApiResponse response = new ApiResponse(false, "An unexpected error occurred.");
-        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        log.error(ex.getMessage(), ex);
+        ApiResponse response = new ApiResponse(false, "An unexpected internal server error occurred.");
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR); // 500
     }
 }
