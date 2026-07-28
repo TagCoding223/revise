c4323ec199e5e0cc9b72438913d7bf03ab8d1478
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-28 08:32:49 +0530

    (frontend) install @react-oauth/google and wrap the app in GoogleOAuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,23 @@ import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { HelmetProvider } from 'react-helmet-async';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Use the exact same Client ID we put in your Spring Boot application.properties
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_KEY
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <ThemeProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </ThemeProvider>
-      </BrowserRouter>
-    </HelmetProvider>
+    <GoogleOAuthProvider clientId='{GOOGLE_CLIENT_ID}'>
+      <HelmetProvider>
+        <BrowserRouter>
+          <ThemeProvider>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </ThemeProvider>
+        </BrowserRouter>
+      </HelmetProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
