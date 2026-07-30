a9585743f04982d1089cfae767aa0ded7bab33cf
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-07-30 21:01:41 +0530

    (backend) RefreshToken Entity

---
+++ backend/src/main/java/com/revise/entity/RefreshToken.java
@@ -0,0 +1,31 @@
+package com.revise.entity;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class RefreshToken {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private Instant expiryDate;
+}
