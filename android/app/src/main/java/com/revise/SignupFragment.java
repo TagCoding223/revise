42e1ce7358a70d4f6e7db53550b5aef9ba3488ed
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-08-02 14:45:59 +0530

    (android) Activate Login button on Signup page

--- android/app/src/main/java/com/revise/SignupFragment.java
+++ android/app/src/main/java/com/revise/SignupFragment.java
@@ -2,11 +2,15 @@ package com.revise;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SignupFragment extends Fragment {
 
@@ -19,5 +23,18 @@ public class SignupFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView tvLoginLink = view.findViewById(R.id.tvLoginLink);
+
+        // Navigate back to LoginFragment when clicked
+        tvLoginLink.setOnClickListener(v -> {
+            // Using the action we defined in nav_graph.xml to ensure the back stack clears properly
+            Navigation.findNavController(view).navigate(R.id.action_signupFragment_to_loginFragment);
+        });
     }
 }
