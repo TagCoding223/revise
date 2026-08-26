232e109e5020c05975c81a873a82e797929f6fd7
Vishal Chouhan <vishalchouhanv.c22.3@gmail.com>
2026-08-26 23:22:54 +0530

    (android) create local database room

---
+++ android/app/src/main/java/com/revise/database/AppDatabase.java
@@ -0,0 +1,31 @@
+package com.revise.database;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+import com.revise.model.Topic;
+
+@Database(entities = {Topic.class}, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract TopicDao topicDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "revise_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
